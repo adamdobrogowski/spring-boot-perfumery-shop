e87581848598c041201fbac020233d2580150630
adamdobrogowski <adam.dobro01@gmail.com>
2025-09-23 15:05:51 +0200

    feat(UI): Redesign user layout

--- src/main/java/pl/perfumeria/perfumery/controller/AuthController.java
+++ src/main/java/pl/perfumeria/perfumery/controller/AuthController.java
@@ -1,6 +1,5 @@
 package pl.perfumeria.perfumery.controller;
 
-
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,15 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.perfumeria.perfumery.dto.UserRegistrationDto;
+import pl.perfumeria.perfumery.repository.UserRepository;
 import pl.perfumeria.perfumery.service.UserService;
 
 @Controller
 public class AuthController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
 
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/register")
@@ -27,17 +29,23 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public String processRegistration(@Valid @ModelAttribute("userDto") UserRegistrationDto userDto, BindingResult bindingResult) {
+    public String processRegistration(@Valid @ModelAttribute("userDto") UserRegistrationDto userDto,
+                                      BindingResult bindingResult) {
+
+        if (userRepository.existsByEmail(userDto.getEmail())) {
+            bindingResult.rejectValue("email", "email.exists", "Użytkownik z tym adresem e-mail już istnieje.");
+        }
+
         if (bindingResult.hasErrors()) {
             return "registration-form";
         }
+
         userService.registerUser(userDto);
-        return "redirect:/login";
+        return "redirect:/login?register_success";
     }
 
     @GetMapping("/login")
     public String showLoginForm() {
         return "login";
     }
-
 }
